a598ad8ec57f9459d7e0904bda3ed4e7ac922f9c
ayush-sr02 <ayushsh2002@gmail.com>
2022-10-09 18:13:54 +0530

    added more solutions to weekly 314 folder

---
+++ weekly-314/2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int n=pref.size();
+        vector<int> ans(n);
+        int cur=pref[0];
+        ans[0]=(pref[0]);
+        for(int i=1;i<n;i++){
+            ans[i] = pref[i]^pref[i-1];
+        }
+        return ans;
+    }
+};
