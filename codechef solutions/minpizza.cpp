70c2e03760fe8c4e68fa30b614269513fe3234fb
Siddharth Ojha <siddharthojha2002@gmail.com>
2022-10-11 23:44:01 +0530

    added more files in codechef solutions folder and optimised previous solutions

---
+++ codechef solutions/minpizza.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    double n,x;
+    cin>>n>>x;
+    int d=ceil((n*x)/4);
+    cout<<d<<" ";
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();cout<<endl;
+    }
+}
